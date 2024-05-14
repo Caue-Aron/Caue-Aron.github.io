8e0a12debf1212a7228c853e7792642bf3326487
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2024-05-14 17:53:06 -0300

    about me section

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const headerBtnList = document.querySelectorAll('.header-button');
+// const headerBtnList = document.querySelectorAll('.header-button');
+// document.querySelector('#header-button-home').classList.add('header-button-focused');
 
-headerBtnList.forEach(headerBtn => {
-    headerBtn.addEventListener('click', () => {
-        document.querySelector('.header-button-focused')?.classList.remove('header-button-focused')
-        headerBtn.classList.add('header-button-focused');
-    });
-})
+// headerBtnList.forEach(headerBtn => {
+//     headerBtn.addEventListener('click', () => {
+//         document.querySelector('.header-button-focused')?.classList.remove('header-button-focused')
+//         headerBtn.classList.add('header-button-focused');
+//     });
+// })
